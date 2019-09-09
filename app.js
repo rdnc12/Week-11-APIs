4017d1a738bc8f72587d4b96e1c64fc2fc1cc42a
Erdinc <Erdinc@gklfmg.com>
2019-09-09 22:20:01 +0200

    searchs are returned with json file

--- app.js
+++ app.js
@@ -18,22 +18,29 @@ app.get('/', (req, res) => {
     res.sendfile(__dirname + '/index.html');
 });
 
-app.post('/', (req, res)=>{
+app.post('/', (req, res) => {
 
-    let searchbox=req.body.searchbox;
+    let searchbox = req.body.searchbox;
     let jsonData = JSON.stringify(searchbox);
-    let options = {
-        url: 'https://en.wikipedia.org/w/api.php',
-        method: 'GET',
-        body: jsonData
-    };
+    let url = `https://en.wikipedia.org/w/api.php?action=opensearch&search= + ${searchbox}`;
 
-    request(options,(error,response,body)=>{
 
-        if (response.statusCode === 200)
-            res.send('success');
+    request(url, (error, response, body) => {
+
+        if (response.statusCode === 200) {
+            var wiki = JSON.parse(body);
+            for (var i = 0; i < wiki[1].length; i++) {
+                var searchData = `You searched for ${wiki[1][i]}`+":";
+                var searchDetails=` And these are the  details— ${wiki[2][i]}`;
+                var searchLink=`Follow this link to read more — ${wiki[3][i]}`;
+                
+                 res.write(data);
+            }
+            res.end();
+            //res.redirect(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchbox}`);
+        }
         else
-            res.send('Problem!!!')
+            res.send('Problem!!!');
     });
 
 });
