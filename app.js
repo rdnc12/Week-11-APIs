9d0a0955d2a11ebf17431f5491136082aa596a25
Erdinc <Erdinc@gklfmg.com>
2019-09-09 23:16:12 +0200

    some changes in js--

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const bodyparser = require('body-parser');
 const request = require('request');
+const fs = require('fs');
 
 const app = express();
 
@@ -22,22 +23,24 @@ app.post('/', (req, res) => {
 
     let searchbox = req.body.searchbox;
     let jsonData = JSON.stringify(searchbox);
-    let url = `https://en.wikipedia.org/w/api.php?action=opensearch&search= + ${searchbox}`;
+    let url = "https://en.wikipedia.org/w/api.php?action=opensearch&search="+searchbox;
 
 
     request(url, (error, response, body) => {
 
         if (response.statusCode === 200) {
-            var wiki = JSON.parse(body);
+
+            let wiki = JSON.parse(body);
             for (var i = 0; i < wiki[1].length; i++) {
-                var searchData = `You searched for ${wiki[1][i]}`+":";
-                var searchDetails=` And these are the  details— ${wiki[2][i]}`;
-                var searchLink=`Follow this link to read more — ${wiki[3][i]}`;
-                
-                 res.write(data);
+                var searchData = wiki[1][i];
+                var searchDetails = wiki[2][i].substring(0,71)+"...";
+                var searchLink = wiki[3][i];
+
+                res.write(searchData+'\n');
+                res.write(searchDetails + '\n');
+                res.write(searchLink + '\n\n');
             }
-            res.end();
-            //res.redirect(`https://en.wikipedia.org/w/api.php?action=opensearch&search=${searchbox}`);
+            res.end();  
         }
         else
             res.send('Problem!!!');
