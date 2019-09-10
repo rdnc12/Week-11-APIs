2708057a3e81079b224ff8b934bb770059c1b4c4
Erdinc <Erdinc@gklfmg.com>
2019-09-11 01:09:53 +0200

    searching is working

--- withXML/app.js
+++ withXML/app.js
@@ -1,2 +1,38 @@
 //jshint esversion:6
 
+$("#searchButton").on('click', () => {
+
+    let searchValue = $("#searchValue").val();
+    const url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchValue;
+    const UList=$("#UList");
+    event.preventDefault();
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+
+    xhr.onload = () => {
+        fetch(url).then(res => res.json()).
+        then(() => { 
+
+            var wiki = JSON.parse(xhr.responseText);
+            for (var i = 0; i < wiki[1].length; i++) {
+                var searchData = wiki[1][i];
+                var searchDetails = wiki[2][i].substring(0, 71) + "...";
+                var searchLink = wiki[3][i];
+                UList.append(`<li class="list-group-item">${i+1}-) 
+                ${searchData}
+                <p>${searchDetails}</p>
+                <a href="${searchLink}">Continue to read</a>
+                </li>`);
+                //console.log(searchData + "\n" + searchDetails + "\n" + searchLink + "\n" + "\n");
+            }
+
+         }).catch(err => console.error(err));
+
+    };
+
+    if(UList.has("li"))
+        UList.empty();
+
+    xhr.send();
+});
