3414701dbd40d2a413c6870210265c79eff6a213
Erdinc <Erdinc@gklfmg.com>
2019-09-11 01:46:45 +0200

    arrange html and css files

--- withXML/app.js
+++ withXML/app.js
@@ -4,7 +4,8 @@ $("#searchButton").on('click', () => {
 
     let searchValue = $("#searchValue").val();
     const url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + searchValue;
-    const UList=$("#UList");
+    const UList = $("#UList");
+
     event.preventDefault();
 
     var xhr = new XMLHttpRequest();
@@ -12,26 +13,25 @@ $("#searchButton").on('click', () => {
 
     xhr.onload = () => {
         fetch(url).then(res => res.json()).
-        then(() => { 
+            then(() => {
 
-            var wiki = JSON.parse(xhr.responseText);
-            for (var i = 0; i < wiki[1].length; i++) {
-                var searchData = wiki[1][i];
-                var searchDetails = wiki[2][i].substring(0, 151) + "...";
-                var searchLink = wiki[3][i];
-                UList.append(`<li class="list-group-item">${i+1}-) 
+                var wiki = JSON.parse(xhr.responseText);
+
+                for (var i = 0; i < wiki[1].length; i++) {
+
+                    var searchData = wiki[1][i];
+                    var searchDetails = wiki[2][i].substring(0, 151) + "...";
+                    var searchLink = wiki[3][i];
+                    UList.append(`<li class="list-group-item">${i + 1}-) 
                 ${searchData}
                 <p>${searchDetails}</p>
                 <a href="${searchLink}">Continue to read</a>
                 </li>`);
-                //console.log(searchData + "\n" + searchDetails + "\n" + searchLink + "\n" + "\n");
-            }
-
-         }).catch(err => console.error(err));
-
+                    //console.log(searchData + "\n" + searchDetails + "\n" + searchLink + "\n" + "\n");
+                }
+            }).catch(err => console.error(err));
     };
-
-    if(UList.has("li"))
+    if (UList.has("li"))
         UList.empty();
 
     xhr.send();
